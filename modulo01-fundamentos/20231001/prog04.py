40e0f0e133ae9906bd1b5cd213f1981746263821
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-10-01 16:31:45 -0300

    Arquivos aula 01 Python web

---
+++ modulo01-fundamentos/20231001/prog04.py
@@ -0,0 +1,13 @@
+# Variáveis e tipos de dados
+# Tipos booleanos (True e False)
+
+if __name__ == "__main__":
+    
+    nome = input("Informe o seu nome: ")
+    idade = int(input("Informe a sua idade: "))
+
+    if idade < 21:
+        print("Você ainda é menor de idade!")
+
+    else:
+        print("Você já é maior de idade.")

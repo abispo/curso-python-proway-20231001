7634e34183183b76a5189a63d82518e59f1a7d6e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-10-01 18:08:36 -0300

    Arquivos aula 01 Python fundamentos

---
+++ modulo01-fundamentos/20231001-aula01/prog08.py
@@ -0,0 +1,39 @@
+# Laço de condição if...elif...else
+
+"""
+O laço if...elif...else faz parte das bases de qualquer linguagem de programação. Com ele possamos verificar valores e condições que
+façam parte de qualquer algoritmo. Por isso o seu entendimento é fundamental.
+
+"""
+
+# Quando criamos variáveis com todas as letras em maiúsculo dentro do Python, indicamos que essas variáveis devem ser tratadas
+# como constantes, ou seja, variáveis que não mudam de valor no decorrer do programa.
+# Como o Python não possui uma palavra reservada para garantir que o valor não seja alterado (const no C#, por exemplo), isso faz parte
+# das convenções de código do Python.
+VISITANTE = 1
+USUARIO = 2
+ADMIN = 3
+
+if __name__ == "__main__":
+    
+    mensagem = """
+    Bem-vindo. Informe o código do tipo de usuário para continuar
+
+    Visitante (1) - Usuario (2) - Administrador (3)
+"""
+
+    print(mensagem)
+
+    codigo = int(input("Informe o código: "))
+
+    if codigo == VISITANTE:
+        print("Identificado como visitante. Acesso somente leitura")
+
+    elif codigo == USUARIO:
+        print("Identificado como usuário. Permissões comuns no sistema")
+    
+    elif codigo == ADMIN:
+        print("Identificado como administrador. Permissão total de acesso")
+
+    else:
+        print("Código de usuário desconhecido. Sem permissões")

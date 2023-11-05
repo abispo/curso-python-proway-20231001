ec1d109edda50706e90e98075d6bfdeaac28d721
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-11-05 16:27:09 -0300

    desafio 01

---
+++ modulo02-python-com-banco-de-dados/20231105-aula01/desafio01.py
@@ -0,0 +1,24 @@
+"""
+Ler o arquivo `cursos.csv` e salvar os dados na tabela `tb_cursos`. Essa tabela deve ter a seguinte estrutura.
+
+```
+id              INT     PRIMARY KEY AUTOINCREMENT
+curso           TEXT    NOT NULL
+carga_horaria   INT     NOT NULL
+preco           REAL    NOT NULL
+```
+
+Após os dados terem sido salvos, vamos salvar um novo arquivo chamado `estatisticas_cursos.txt`, que terá o formato a seguir:
+
+```
+Estatísticas dos cursos
+
+Quantidade de cursos: 10
+Curso com a maior carga horária: Web Development com Django (50 horas)
+Curso com o maior valor: Machine Learning Fundamentals (R$ 1200.00)
+
+```
+"""
+
+if __name__ == "__main__":
+    pass

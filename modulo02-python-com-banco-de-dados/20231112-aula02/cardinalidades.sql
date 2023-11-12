5fa3e2ca419c6e9915466f7a68453d24e4664015
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-11-12 18:40:28 -0300

    Arquivos aula 02 Python com Banco de Dados.

--- modulo02-python-com-banco-de-dados/20231112-aula02/cardinalidades.sql
+++ modulo02-python-com-banco-de-dados/20231112-aula02/cardinalidades.sql
@@ -96,3 +96,66 @@ da tabela tb_usuarios na tabela tb_postagens. Na esmagadora maioria dos
 casos, a tabela que possui a coluna de chave estrangeira será o lado N
 da relação Um para Muitos (1:N).
 */
+
+/*
+Deve ser possível associar categorias às postagens, de uma maneira similar
+ao funcionamento das hashtags no twitter. Para isso, vamor criar a tabela
+tb_categorias
+*/
+
+CREATE TABLE IF NOT EXISTS tb_categorias(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(200) NOT NULL
+);
+
+INSERT INTO tb_categorias (nome) VALUES
+	("SQL"),
+    ("Bancos de Dados"),
+    ("Proway"),
+    ("Programação"),
+    ("Python");
+SELECT * FROM tb_categorias;
+/*
+A regra entre postagens e categorias é a seguinte: Uma postagem pode ter
+diversas categorias associadas a ela, e uma categoria pode aparecer em
+várias postagens.
+
+Nesse cenário, a regra a se fazer é criar uma tabela associativa entre
+tb_postagens e tb_categorias. Essa tabela associativa terá as chaves
+estrangeiras para postagens e categorias, assim sendo possível implementar
+essa regra de negócio.
+*/
+
+-- Criar a tabela associativa entre postagens e categorias
+CREATE TABLE IF NOT EXISTS tb_postagens_categorias(
+	postagem_id INT NOT NULL,
+    categoria_id INT NOT NULL,
+    
+    PRIMARY KEY(postagem_id, categoria_id),
+    FOREIGN KEY(postagem_id) REFERENCES tb_postagens(id),
+    FOREIGN KEY(categoria_id) REFERENCES tb_categorias(id)
+);
+
+INSERT INTO tb_postagens_categorias(postagem_id, categoria_id) VALUES
+	(1, 3),
+    (1, 4),
+    (1, 5),
+    (2, 3),
+    (2, 4);
+SELECT * FROM tb_postagens_categorias;
+
+-- Mostrando as postagens e as categorias associadas a elas
+SELECT tbp.id, tbp.titulo, tbc.nome FROM tb_postagens tbp
+INNER JOIN tb_postagens_categorias tbpc
+ON tbp.id = tbpc.postagem_id
+INNER JOIN tb_categorias tbc
+ON tbpc.categoria_id = tbc.id;
+
+-- Mostrando em quantas postagens uma hashtag aparece
+SELECT tbc.id, tbc.nome, COUNT(tbp.id) as ocorrencias FROM tb_categorias tbc
+LEFT JOIN tb_postagens_categorias tbpc
+ON tbc.id = tbpc.categoria_id
+LEFT JOIN tb_postagens tbp
+ON tbpc.postagem_id = tbp.id
+GROUP BY tbc.id
+ORDER BY ocorrencias DESC;

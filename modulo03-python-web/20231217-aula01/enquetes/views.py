a2ce52aaa01a96a748687e6e341d25965e323fb7
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-01-07 15:58:38 -0300

    arquivos aula 02 python web

--- modulo03-python-web/20231217-aula01/enquetes/views.py
+++ modulo03-python-web/20231217-aula01/enquetes/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Pergunta
+from .models import Opcao, Pergunta
 
 def index(request):
     todas_as_perguntas = Pergunta.objects.order_by("-data_publicacao")
@@ -22,4 +23,25 @@ def resultados(request, pergunta_id):
 
 
 def votar(request, pergunta_id):
-    return HttpResponse(f"Você está votando em uma opção da pergunta {pergunta_id}")
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    try:
+        opcao_escolhida = pergunta.opcao_set.get(pk=request.POST["opcao"])
+
+    except (KeyError, Opcao.DoesNotExist):
+        return render(
+            request,
+            "enquetes/detalhes.html",
+            {
+                "pergunta": pergunta,
+                "mensagem_erro": "Você deve escolher uma opção!"
+            }
+        )
+
+    else:
+        opcao_escolhida.votos += 1
+        opcao_escolhida.save()
+
+        return HttpResponseRedirect(
+            reverse("enquetes:resultados", args=(pergunta.id,))
+        )

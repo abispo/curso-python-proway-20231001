722c8870efd6c46463b73f47865e62e8594822ff
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-01-14 15:14:50 -0300

    Arquivos aula 03 python web

--- modulo03-python-web/20240114-aula03/agendamento/settings.py
+++ modulo03-python-web/20240114-aula03/agendamento/settings.py
@@ -10,17 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Carregar as informações do arquivo .env como variáveis de ambiente
+load_dotenv()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&f=8%9ob=ml!pzzj3w2_dhmkrmz#1-69p(7ju*!au@12k-^jpj'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

f19ac3141983fa61384f132424c0bc81149317aa
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-02-25 15:01:23 -0300

    Arquivos aula 08 python web

--- modulo03-python-web/20240225-aula08/registro/apps.py
+++ modulo03-python-web/20240225-aula08/registro/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RegistroConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'registro'
+
+    def ready(self) -> None:
+        import registro.signals
--- modulo03-python-web/20240225-aula08/registro/models.py
+++ modulo03-python-web/20240225-aula08/registro/models.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 
+from django.contrib.auth.models import User
 from django.db import models
 
 class PreRegistro(models.Model):
@@ -11,3 +12,18 @@ class PreRegistro(models.Model):
 
     class Meta:
         db_table = "tb_pre_registro"
+
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True
+    )
+    tipo_logradouro = models.CharField(max_length=50, null=True, blank=True)
+    logradouro = models.CharField(max_length=200, null=True, blank=True)
+    numero = models.CharField(max_length=10, null=True, blank=True)
+    bairro = models.CharField(max_length=100, null=True, blank=True)
+    cidade = models.CharField(max_length=200, null=True, blank=True)
+    uf = models.CharField(max_length=2, null=True, blank=True)
+
+    class Meta:
+        db_table = "tb_perfis"

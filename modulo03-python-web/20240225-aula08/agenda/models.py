487dba2327b3c3b17e6c4669a9a9b41a58322e88
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-02-25 15:56:26 -0300

    Arquivos aula 08 python web

--- modulo03-python-web/20240225-aula08/agenda/models.py
+++ modulo03-python-web/20240225-aula08/agenda/models.py
@@ -3,4 +3,29 @@ from django.db import models
 # Model que representa a tabela auth_user
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Servico(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=200)
+    habilitado = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "tb_servicos"
+
+
+class Agenda(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    servico = models.ForeignKey(Servico, on_delete=models.SET_NULL, null=True)
+    data_hora_inicial = models.DateTimeField()
+    data_hora_final = models.DateTimeField()
+
+    class Meta:
+        db_table = "tb_agendamentos"
+
+
+class StatusAgendamento(models.Model):
+    agenda = models.OneToOneField(Agenda, on_delete=models.CASCADE, primary_key=True)
+    status = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = "tb_status_agendamentos"
